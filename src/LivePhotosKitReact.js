ec956a3bfb7221839a907847500d964e1eacc7a9
henteko <henteko07@gmail.com>
2017-04-22 17:59:09 +0900

    add lint

--- src/LivePhotosKitReact.js
+++ src/LivePhotosKitReact.js
@@ -1,11 +1,9 @@
-'use strict';
-
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as LivePhotosKit from 'livephotoskit';
 
 class LivePhotosKitReact extends Component {
-  componentDidMount() {
+  componentDidMount () {
     const {
       photoSrc,
       videoSrc,
@@ -15,9 +13,9 @@ class LivePhotosKitReact extends Component {
       photoLoad,
       videoLoad,
       playbackStyle,
-      style
+      style,
     } = this.props;
-    const _style = {width: '100%', height: '100%', ...style};
+    const _style = { width: '100%', height: '100%', ...style };
 
     this._player = LivePhotosKit.Player(this._node);
 
@@ -25,63 +23,67 @@ class LivePhotosKitReact extends Component {
     this._player.videoSrc = videoSrc;
     this._player.addEventListener('canplay', evt => {
       if (canPlay === undefined) { return; }
-      canPlay(evt)
+      canPlay(evt);
     });
     this._player.addEventListener('error', evt => {
       if (error === undefined) { return; }
-      error(evt)
+      error(evt);
     });
     this._player.addEventListener('ended', evt => {
       if (ended === undefined) { return; }
-      ended(evt)
+      ended(evt);
     });
     this._player.addEventListener('videoload', evt => {
       if (videoLoad === undefined) { return; }
-      videoLoad(evt)
+      videoLoad(evt);
     });
     this._player.addEventListener('photoload', evt => {
       if (photoLoad === undefined) { return; }
-      photoLoad(evt)
+      photoLoad(evt);
     });
     this._player.playbackStyle = playbackStyle;
 
     for (var key in _style) {
-      this._player.style[key] = _style[key];
+      if (_style.hasOwnProperty(key)) {
+        this._player.style[key] = _style[key];
+      }
     }
   }
 
-  play() {
+  play () {
     this._player.play();
   }
 
-  pause() {
+  pause () {
     this._player.play();
   }
 
-  toggle() {
+  toggle () {
     this._player.toggle();
   }
 
-  stop() {
+  stop () {
     this._player.stop();
   }
 
-  seekTo(currentTime) {
+  seekTo (currentTime) {
     this._player.currentTime = currentTime;
   }
 
-  get livephotoskit() {
+  get livephotoskit () {
     return this._player;
   }
 
-  get duration() {
+  get duration () {
     return this._player.duration;
   }
 
-  render() {
-    return(
+  render () {
+    return (
       <div
-        ref={c => this._node = c}
+        ref={ c => {
+          return (this._node = c);
+        } }
       ></div>
     );
   }
@@ -96,7 +98,7 @@ LivePhotosKitReact.propTypes = {
   photoLoad: PropTypes.func,
   videoLoad: PropTypes.func,
   playbackStyle: PropTypes.string,
-  style: PropTypes.object
+  style: PropTypes.object,
 };
 
 export default LivePhotosKitReact;
